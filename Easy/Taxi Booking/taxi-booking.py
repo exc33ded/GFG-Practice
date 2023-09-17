29ae49ab3ecd619dabbb11078de759eb4ba8b025
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-09-17 12:17:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taxi Booking/taxi-booking.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+class Solution:
+    def minimumTime(self, N : int, cur : int, pos : List[int], time : List[int]) -> int:
+        # code here
+        lst = []
+        min_ = 0
+        for i in range(N):
+            tt = abs((pos[i] - cur) * time[i])
+            lst.append(tt)
+        return min(lst)
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        cur = int(input())
+        
+        
+        pos=IntArray().Input(N)
+        
+        
+        time=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.minimumTime(N, cur, pos, time)
+        
+        print(res)
+        
+
+# } Driver Code Ends
