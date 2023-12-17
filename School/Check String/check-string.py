6f71bf0cd1f951204a66c63586ec400e002214fb
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-12-18 00:17:11 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check String/check-string.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def check (self,s):
+        # your code here
+        a = set(s)
+        if len(a) == 1:
+            return 1
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    s = input ()
+    ob = Solution()
+    if ob.check (s):
+        print ("YES")
+    else:
+        print ("NO")
+        
+# Contributed By: Pranay Bansal
+
+# } Driver Code Ends
