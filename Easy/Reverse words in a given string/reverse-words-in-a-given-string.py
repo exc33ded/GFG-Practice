31bad9a3a919de14a0d224628fb5a63259155812
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-01-09 22:59:37 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reverse words in a given string/reverse-words-in-a-given-string.py
+++ Easy/Reverse words in a given string/reverse-words-in-a-given-string.py
@@ -5,9 +5,9 @@ class Solution:
     #Function to reverse words in a given string.
     def reverseWords(self,S):
         # code here 
-        return ".".join(S.split(".")[::-1])
-        
-
+        lst = S.split(".")
+        rev = lst[::-1]
+        return ".".join(rev)
 
 #{ 
  # Driver Code Starts
