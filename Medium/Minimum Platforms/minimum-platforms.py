5043d65b28783a6035b5bf8d0118d3efaa386cb8
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-15 17:36:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Platforms/minimum-platforms.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to find the minimum number of platforms required at the
+    #railway station such that no train waits.
+    def minimumPlatform(self,n,arr,dep):
+        # code here
+        arr.sort()
+        dep.sort()
+        count=1
+        max=1
+        i=1
+        j=0
+        while i<n and j<n:
+            if arr[i]<=dep[j]:
+                i+=1
+                count+=1
+                if count> max:
+                    max=count
+            else:
+                j+=1
+                count-=1
+        return max
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        arrival = list(map(int, input().strip().split()))
+        departure = list(map(int, input().strip().split()))
+        ob=Solution()
+        print(ob.minimumPlatform(n,arrival,departure))
+# } Driver Code Ends
