3fde54610b9b3fdce0abc37f7ad06cc91af1973c
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-03-14 23:22:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest subsquare surrounded by X/largest-subsquare-surrounded-by-x.py
+++ Medium/Largest subsquare surrounded by X/largest-subsquare-surrounded-by-x.py
@@ -3,29 +3,40 @@
 class Solution:
     def largestSubsquare(self, n, A):
         #code here
-        ROWS,COLS = len(A),len(A[0])
-        dp = [[[0,0] for i in range(ROWS+1)] for j in range(COLS+1)]
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                if A[i][j] == 'X':
-                    dp[i+1][j+1][0] = dp[i][j+1][0] + 1
-                    dp[i+1][j+1][1] = dp[i+1][j][1] + 1
-                    
-                    
-        maxi = 0
-        
-        for i in range(ROWS,0,-1):
-            for j in range(COLS,0,-1):
-                curMin = min(dp[i][j][0],dp[i][j][1])
-                while curMin > maxi:
-                    if dp[i-curMin+1][j][1] >= curMin and dp[i][j-curMin+1][0] >= curMin:
-                        maxi = curMin
-                        
+        by_rows = [[0] * n for _ in range(n)]
+        by_cols = [[0] * n for _ in range(n)]
+
+        # Calculate the number of 'X' horizontally for each row
+        for i in range(n):
+            by_rows[i][0] = 1 if a[i][0] == 'X' else 0
+            for j in range(1, n):
+                if a[i][j] == 'X':
+                    by_rows[i][j] = by_rows[i][j - 1] + 1
+                else:
+                    by_rows[i][j] = 0
+
+        # Calculate the number of 'X' vertically for each column
+        for j in range(n):
+            by_cols[0][j] = 1 if a[0][j] == 'X' else 0
+            for i in range(1, n):
+                if a[i][j] == 'X':
+                    by_cols[i][j] = by_cols[i - 1][j] + 1
+                else:
+                    by_cols[i][j] = 0
+
+        res = 0
+
+        # Iterate over each cell from bottom right to top left
+        for i in range(n - 1, res - 1, -1):
+            for j in range(n - 1, res - 1, -1):
+                side = min(by_rows[i][j], by_cols[i][j])
+                while side > res:
+                    # Check if a subsquare of size 'side' can be formed
+                    if by_rows[i - side + 1][j] >= side and by_cols[i][j - side + 1] >= side:
+                        res = side
                     else:
-                        curMin -= 1
-                        
-        return maxi
+                        side -= 1
+        return res
 
 #{ 
  # Driver Code Starts
