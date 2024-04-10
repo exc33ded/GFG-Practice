329f5f03c147d529c28b57fc4ac2095bb68f681a
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-04-11 00:06:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Gray to Binary equivalent/gray-to-binary-equivalent.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:    
+    ##Complete this function
+    # function to convert a given Gray equivalent n to Binary equivalent.
+    def grayToBinary(self,n):
+        ##Your code here
+        res = n
+        while n > 0:
+            n >>=1
+            res ^= n
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        print(ob.grayToBinary(n))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
