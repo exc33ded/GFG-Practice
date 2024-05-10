26ca580cbea1053964c294558cf00c985569cf30
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-05-11 00:06:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Juggler Sequence/juggler-sequence.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def jugglerSequence(self, n):
+        # code here
+        sequence=[n]
+        while n!=1:
+            if n%2==0:
+                  n = int(n ** (1/2))
+            else:
+                n = int(n ** (3/2))
+
+            sequence.append(n)
+        return sequence
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+
+        ob = Solution()
+        arr = ob.jugglerSequence(n)
+        for i in (arr):
+            print(i, end=" ")
+        print()
+
+# } Driver Code Ends
