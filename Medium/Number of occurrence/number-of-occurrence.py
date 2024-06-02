094522e0c75e64066355dc8d916cd0eeacacb6df
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-06-02 20:44:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.py
+++ Medium/Number of occurrence/number-of-occurrence.py
@@ -1,16 +1,44 @@
 #User function Template for python3
 class Solution:
-
 	def count(self,arr, n, x):
 		# code here
-		
-		# Brute Force
-		counter = 0
-		for i in arr:
-            if i == x:
-                counter += 1
-        
-        return counter
+		def floor(arr, n, x):
+		    start = 0
+		    end = n - 1
+		    res = -1
+		    while(start <= end):
+		        mid = start + (end - start)//2
+		        if arr[mid] == x:
+		            res = mid
+		            start = mid + 1
+		        elif arr[mid] > x:
+		            end = mid - 1
+                else:
+                    start = mid + 1
+            return res
+            
+        def ceil(arr, n, x):
+		    start = 0
+		    end = n - 1
+		    res = -1
+		    while(start <= end):
+		        mid = start + (end - start)//2
+		        if arr[mid] == x:
+		            res = mid
+		            end = mid - 1
+		        elif arr[mid] > x:
+		            end = mid - 1
+                else:
+                    start = mid + 1
+            return res
+            
+        low = floor(arr, n, x)
+        high = ceil(arr, n, x)
+
+        if low == -1:
+            return 0
+        else:
+            return abs(high - low - 1)
 
 #{ 
  # Driver Code Starts
