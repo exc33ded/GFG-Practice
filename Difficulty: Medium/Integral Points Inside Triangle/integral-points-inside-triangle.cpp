d634d80db69e5352d0a3f5d4f5c9cc371cbb4d47
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-06-20 00:20:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Integral Points Inside Triangle/integral-points-inside-triangle.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    long long int gcd(long long int a, long long int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    long long int boundaryPoints(long long int x1, long long int y1, long long int x2, long long int y2) {
+        return gcd(abs(x2 - x1), abs(y2 - y1)) + 1;
+    }
+
+    long long int InternalCount(long long int p[], long long int q[], long long int r[]) {
+         long long int area = abs(p[0] * (q[1] - r[1]) + q[0] * (r[1] - p[1]) + r[0] * (p[1] - q[1])) / 2;
+        
+         long long int B = boundaryPoints(p[0], p[1], q[0], q[1]) +
+                          boundaryPoints(q[0], q[1], r[0], r[1]) +
+                          boundaryPoints(r[0], r[1], p[0], p[1]) - 3;
+
+         long long int I = area - B / 2 + 1;
+
+        return I;
+    }
+}; 
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int p[2], q[2], r[2];
+        cin >> p[0] >> p[1] >> q[0] >> q[1] >> r[0] >> r[1];
+        Solution ob;
+        long long int ans = ob.InternalCount(p, q, r);
+        cout << ans << "\n";
+    }
+}
+// } Driver Code Ends
