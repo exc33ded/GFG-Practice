6292f99d3035af1bd5210728cc311334cfa7e76f
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-06-30 00:03:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.py
@@ -0,0 +1,73 @@
+class Solution:
+    def delete_node(self, head, x):
+        #code here
+        if x == 1:
+            head = head.next
+            head.prev = None
+            return head
+        
+        h = head
+        for i in range(1,x):
+            h = h.next
+        
+        if h.next is None:
+            h.prev.next = None
+            return head
+        h.prev.next, h.next.prev = h.next, h.prev
+        
+        return head
+        
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    @staticmethod
+    def print_list(node):
+        while node is not None:
+            print(node.data, end=" ")
+            node = node.next
+        print()
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    index = 0
+    t = int(data[index])
+    index += 1
+    for _ in range(t):
+        n = int(data[index])
+        index += 1
+        head = None
+        tail = head
+
+        for i in range(n):
+            temp = int(data[index])
+            index += 1
+            if head is None:
+                head = Node(temp)
+                tail = head
+            else:
+                new_node = Node(temp)
+                tail.next = new_node
+                new_node.prev = tail
+                tail = new_node
+
+        x = int(data[index])
+        index += 1
+
+        obj = Solution()
+        res = obj.delete_node(head, x)
+
+        Node.print_list(res)
+
+# } Driver Code Ends
