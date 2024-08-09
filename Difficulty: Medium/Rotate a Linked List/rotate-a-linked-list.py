ce433882fc2351267669bc806d3a0ddfaa84bf3f
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-08-10 00:23:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.py
@@ -0,0 +1,82 @@
+# Your task is to complete this function
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    
+    #Function to rotate a linked list.
+    def rotate(self, head, k):
+        # code here
+        curr=head
+        count=0
+        while count!=k-1:
+            curr=curr.next
+            count+=1
+        new_head=curr.next
+        curr.next=None
+        step=new_head
+        if step:
+            while step.next!=None:
+                step=step.next
+            step.next=head
+            head=new_head
+        return head
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Define the Node class for the linked list
+class Node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+# Function to print the linked list
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+#Position this line where user code will be pasted.
+
+# Main function
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0].strip())
+    idx = 1
+
+    while t > 0:
+        arr = list(map(int, data[idx].strip().split()))
+
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for num in arr[1:]:
+                tail.next = Node(num)
+                tail = tail.next
+
+        k = int(data[idx + 1].strip())
+        idx += 2
+        head = Solution().rotate(head, k)
+        printList(head)
+        t -= 1
+
+# } Driver Code Ends
