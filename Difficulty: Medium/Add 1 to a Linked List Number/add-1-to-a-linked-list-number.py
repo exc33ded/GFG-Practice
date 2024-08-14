15a3ac1809f69fcb632c340f0c1b56b06b1d3a70
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-08-15 00:09:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.py
@@ -0,0 +1,78 @@
+#User function Template for python3
+
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def addOne(self,head):
+        #Returns new head of linked List.
+        s = ""
+        curr = head
+        while curr:
+            s += str(curr.data)
+            curr = curr.next
+
+        new_value = int(s) + 1
+        
+        dummy = Node(-1)
+        curr = dummy
+        for num in str(new_value):
+            curr.next = Node(num)
+            curr = curr.next
+        return dummy.next
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Node Class
+class Node:
+
+    def __init__(self, data):  # data -> value stored in node
+        self.data = data
+        self.next = None
+
+
+# Linked List Class
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+
+
+def PrintList(head):
+    while head:
+        print(head.data, end='')
+        head = head.next
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        list1 = LinkedList()
+        arr = list(map(int, input().strip().split()))
+        for i in arr:
+            list1.insert(i)
+
+        resHead = Solution().addOne(list1.head)
+        PrintList(resHead)
+        print()
+
+# } Driver Code Ends
