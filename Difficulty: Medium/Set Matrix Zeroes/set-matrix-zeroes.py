e826735844224e7cc48228459abc281a3527e34d
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-12-25 00:09:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+class Solution:
+    def setMatrixZeroes(self, matrix):
+        cols = [1]*len(matrix[0])
+        rows = [1]*len(matrix)
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    rows[i] = 0
+                    cols[j] = 0
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if rows[i] == 0 or cols[j] == 0:
+                    matrix[i][j] = 0
+
+
+#{ 
+ # Driver Code Starts
+import sys
+
+# Position this line where user code will be pasted.
+if __name__ == "__main__":
+    input = sys.stdin.read
+    data = input().split()
+
+    idx = 0
+    t = int(data[idx])
+    idx += 1
+    results = []
+
+    for _ in range(t):
+        n, m = map(int, data[idx:idx + 2])
+        idx += 2
+        arr = []
+        for i in range(n):
+            arr.append(list(map(int, data[idx:idx + m])))
+            idx += m
+
+        sol = Solution()
+        sol.setMatrixZeroes(arr)
+
+        for row in arr:
+            results.append(" ".join(map(str, row)))
+
+        results.append("~")
+
+    sys.stdout.write("\n".join(results) + "\n")
+
+# } Driver Code Ends
