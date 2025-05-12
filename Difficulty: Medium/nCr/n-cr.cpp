d41c66d79e0d855755edc9c7ee68e011a98d9352
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2025-05-13 00:24:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        long int ans =1;
+        for(int i=0;i<r;i++){
+            ans=ans*(n-i);
+            ans=ans/(i+1);
+        }
+      return ans;}
+    };
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
