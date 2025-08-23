316af53bb9a937a939c2c480126f45b44981acf6
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2025-08-23 23:03:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPossible(self,arr,val,k):
+        count,curr=1,0
+        for pages in arr:
+            curr+=pages
+            if curr>val:
+                curr=pages
+                count+=1
+            if count>k or pages>val:
+                return False
+        return True
+
+    def findPages(self, arr, k):
+        if len(arr)<k:
+            return -1
+        l,h,ans=0,sum(arr),-1
+        while l<=h:
+            mid=(l+h)//2
+            if self.isPossible(arr,mid,k):
+                ans=mid
+                h=mid-1
+            else:
+                l=mid+1
+        return ans
