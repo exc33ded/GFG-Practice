22c00c05311af09856f26468c7c61cbe0aecef16
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2025-08-25 23:34:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.py
+++ Difficulty: Medium/Rotate Array/rotate-array.py
@@ -2,48 +2,28 @@
 
 class Solution:
     #Function to rotate an array by d elements in counter-clockwise direction. 
-    def rotateArr(self,A,D):
+    def rotateArr(self, arr, d):
         #Your code here
-        N = len(A)
-        if D > N:
-            D = D%N
-        k = A[D:] + A[:D]
+        # n = len(arr)
         
-        for i in range(N):
-            A[i] = k[i]
-
-
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import math
-
-
-def main():
-    T = int(input())
-
-    while (T > 0):
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ob.rotateArr(A, D)
-
-        for i in A:
-            print(i, end=" ")
-
-        print()
-
-        T -= 1
-
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        # d = d % n
+        
+        # def reverse(arr,start, end):
+        #     while start < end:
+        #         arr[start], arr[end] = arr[end], arr[start]
+        #         start += 1
+        #         end -= 1
+        
+        # reverse(arr, 0, d-1)
+        # reverse(arr, d, n-1)
+        # reverse(arr, 0, n-1)
+        
+        n = len(arr)
+        
+        if d > n:
+            d = d % n
+        
+        k = arr[d:] + arr[:d]
+        
+        for i in range(n):
+            arr[i] = k[i]
