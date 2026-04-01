ba099f3e9b34c80f4e938edbed4ebd87cea2d8d7
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2026-04-01 23:52:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.py
@@ -0,0 +1,12 @@
+class Solution:
+	def countStrings(self,n):
+    	# code here
+    	if n<3:
+            return n+1
+        
+        a, b = 2, 3  # dp[1], dp[2]
+        
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+        
+        return b
